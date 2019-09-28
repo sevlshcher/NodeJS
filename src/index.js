e1fc6d72e329ddbd9aa58410b89d7fb6c186fc87
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-09-28 05:20:21 +0300

    remove custom copyFile func and use fs.copyFile

--- src/index.js
+++ src/index.js
@@ -1,51 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const argv = require('yargs').argv
-const rimraf = require('rimraf')
 
 const newFile = __dirname + `/${argv.dir}`
 
 const readDir = (base) => {
   const files = fs.readdirSync(base)
-
   files.forEach(item => {
     let localBase = path.join(base, item)
+    let firstLet = item[0]
     let state = fs.statSync(localBase)
-    if (state.isFile()) {
-      let firstLet = item[0]
-      fs.mkdirSync(path.join(newFile, firstLet), { recursive: true })
-      copyFile(localBase, path.join(newFile, firstLet, item), () => {
-        console.log(`copy file: ${item}`)
-      })
-    } else {
-      readDir(localBase)
-    }
+    state.isFile() ? (
+      fs.mkdirSync(path.join(newFile, firstLet), { recursive: true }),
+      fs.copyFileSync(localBase, path.join(newFile, firstLet, item)),
+      argv._[0] === 'delete' ? fs.unlinkSync(localBase) : null
+    ) : readDir(localBase)
   })
-  if(argv._[0] === 'delete') {
-    rimraf(base, () => {
-      console.log(`delete folder: ${base}`)
-    })
-  }
-}
-
-function copyFile(source, target, cb) {
-  let cbCalled = false
-
-  const rd = fs.createReadStream(source)
-  rd.on("error", err => done (err))
-
-  const wr = fs.createWriteStream(target)
-  wr.on("error", err => done (err))
-    .on("close", () => done (null))
-
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
-  }
+  argv._[0] === 'delete' ? fs.rmdirSync(base) : null
 }
 
 if(argv.base !== undefined && argv.dir !== undefined && (argv._[0] === undefined || argv._[0] === 'delete')) {
