cc32b78bb1a16119394a8590e50b158fc5a938f0
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-09-30 02:25:58 +0300

    some fixies

--- worker.js
+++ worker.js
@@ -1,25 +1,23 @@
 const http = require('http')
-const moment = require('moment')
 const pid = process.pid
 require('dotenv').config()
 
 http
     .createServer((req, res) => {
-        if (req.url !== '/favicon.ico') {
+        if (req.url !== '/favicon.ico' && req.method === 'GET') {
             res.writeHead(200, { 'Content-Type': 'text/html' })
-            
-            let utc = moment.utc().format('YYYY/MM/DD , HH:mm:ss')
-            let count = 0
+            let date = new Date(),
+                dateUTC = date.toUTCString(),
+                count = 0
 
-            const myFunc = () => {
-                console.log(`Current date: ${utc}`)
+            const timer = setInterval(() => {
                 count++
-                if (count === process.env.TIME_OUT / 1000) {
+                console.log(`${count}. Current date: ${dateUTC}`)
+                if (+new Date() - date.getTime() >= process.env.TIME_OUT) {
                     clearInterval(timer)
-                    res.end(`Breakin time: ${utc}`)
+                    res.end(`Breakin time: ${dateUTC}`)
                 }
-            }
-            const timer = setInterval(myFunc, process.env.TIME_INT)
+            }, process.env.TIME_INT)
         }
     })
     .listen(8080, () => {
