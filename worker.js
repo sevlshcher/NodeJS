fc24eff1213a4f01b4c55d0edec6a7c2f600f844
Sergey Shcherbakov <sevlshcher@gmail.com>
2019-10-02 00:48:15 +0300

    redid counter. HW completed+

--- worker.js
+++ worker.js
@@ -2,13 +2,14 @@ const http = require('http')
 const pid = process.pid
 require('dotenv').config()
 
+let count = 0
+
 http
     .createServer((req, res) => {
         if (req.url !== '/favicon.ico' && req.method === 'GET') {
             res.writeHead(200, { 'Content-Type': 'text/html' })
             let date = new Date(),
-                dateUTC = date.toUTCString(),
-                count = 0
+                dateUTC = date.toUTCString()
 
             const timer = setInterval(() => {
                 count++
